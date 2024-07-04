7ea1c4fb147c3414d7de0ca0e1549d6cdee4e288
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-04 12:49:57 -0500

    feat[HU-1(user)] add user persistence adapter which implements user persistence port and its methods: save, get by email and by id

--- src/main/java/com/pragma/userfoodcourt/infrastructure/driven/jpa/mysql/repository/IUserRepository.java
+++ src/main/java/com/pragma/userfoodcourt/infrastructure/driven/jpa/mysql/repository/IUserRepository.java
@@ -1,11 +1,10 @@
 package com.pragma.userfoodcourt.infrastructure.driven.jpa.mysql.repository;
 
-import com.pragma.userfoodcourt.domain.model.User;
 import com.pragma.userfoodcourt.infrastructure.driven.jpa.mysql.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface IUserRepository extends JpaRepository<User, String> {
+public interface IUserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByEmail(String email);
 }

c0d34fccd0e4a649bc034226ec3ae0e305b55b44
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-09 10:44:55 -0500

    refactor[HU-6(register-user)] now role is setted via method params

--- src/main/java/com/pragma/userfoodcourt/application/handler/AuthHandler.java
+++ src/main/java/com/pragma/userfoodcourt/application/handler/AuthHandler.java
@@ -5,6 +5,7 @@ import com.pragma.userfoodcourt.application.dto.request.RegisterOwnerRequest;
 import com.pragma.userfoodcourt.application.dto.response.AuthResponse;
 import com.pragma.userfoodcourt.application.mapper.IAuthRequestMapper;
 import com.pragma.userfoodcourt.domain.api.IAuthServicePort;
+import com.pragma.userfoodcourt.domain.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class AuthHandler {
     private final IAuthRequestMapper authRequestMapper;
 
     public void registerOwner(RegisterOwnerRequest request) {
-        authServicePort.registerRestaurantOwner(authRequestMapper.toModel(request));
+        authServicePort.registerUser(authRequestMapper.toModel(request), Role.OWNER);
     }
 
     public AuthResponse login(LoginRequest request) {
